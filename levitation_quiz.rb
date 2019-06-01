d50e3d15f0797dd0fd4bb4cfabdb3f0daac65c57
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-06-01 20:16:35 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -2,7 +2,7 @@
 def levitation_quiz
 	
     loop do 
-        puts "What's up, Doc?"
+        puts "What is the spell that enacts levitation?"
         answer = gets.chomp
         break if answer == "STOP"
     end
